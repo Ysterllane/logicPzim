4430ea725d03bce5e798b44d426ad33ac8d45845
Ysterllane <131874791+Ysterllane@users.noreply.github.com>
2023-06-03 15:48:25 -0300

    Update Exercício 2.pas

--- 1º Bimestre/Exercício 2.pas
+++ 1º Bimestre/Exercício 2.pas
@@ -3,12 +3,12 @@
 Program Pzim ;
    var nt1, nt2, nt3, ma : real;
 Begin
-	 write('Diga a nota 1:');
+	 write('Diga a nota 1: ');
 	  read(nt1);
-	 write('Diga a nota 2:');
+	 write('Diga a nota 2: ');
 	  read(nt2);
-	 write('Diga a nota 3:');
+	 write('Diga a nota 3: ');
 	  read(nt3);
 ma:= (nt1+nt2+nt3)/3;	 
-   write('Sua média aritimética é: ', ma);
+   write('Sua média aritimética é: ', ma:2:2);
 End.
