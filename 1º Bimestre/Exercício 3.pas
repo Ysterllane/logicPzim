2c1c6754234ea4f8263eaeb4ba450d714c3d61c0
Ysterllane <131874791+Ysterllane@users.noreply.github.com>
2023-06-03 16:00:42 -0300

    Update Exercício 3.pas

--- 1º Bimestre/Exercício 3.pas
+++ 1º Bimestre/Exercício 3.pas
@@ -7,18 +7,22 @@ Begin
 	 write('Diga a nota 1: ');
 	  read(nt1);
 	 write('Diga o peso 1: ');
-	  readln(p1);
+	  read(p1); 
+	 writeln;
 	  
 	 write('Diga a nota 2: ');
 	  read(nt2);
 	 write('Diga o peso 2: ');
-	  readln(p2); 
+	  read(p2); 
+	 writeln; 
 	  
 	 write('Diga a nota 3: ');
-	  readln(nt3);
+	  read(nt3);
 	 write('Diga o peso 3: ');
-	  readln(p3); 
+	  read(p3); 
+	 writeln;
 	  
 mp:= (nt1 * p1 + nt2 * p2 + nt3 * p3)/(p1+p2+p3);	 
    write('Sua média ponderada é: ', mp:2:2);
+    readkey;
 End.
