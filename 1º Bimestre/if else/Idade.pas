1af9fa4cbfc849ab6c35c7075ece68a1e7ce7dc5
Ysterllane <131874791+Ysterllane@users.noreply.github.com>
2023-06-06 14:37:34 -0300

    Utilização do >, if ( ) then, else

--- 1º Bimestre/if else/Idade.pas
+++ 1º Bimestre/if else/Idade.pas
@@ -1,12 +1,10 @@
 Program Melhor_idade ;
 Var Id: integer;
 Begin
-  Readln (Id);
-	
-	if Id > 65
-	 then
-	  write ('Maior de 65')
-		
-	 else
-	  write ('Menor de 65');	   
+ write('Qual sua idade? ');
+  Readln (Id); writeln;
+if Id > 65 then
+  write ('Maior de 65')	
+else
+  write ('Menor de 65');	   
 End.
