73e6a3d8bd8d05f8a482f15132f17d21afbead10
Ysterllane <131874791+Ysterllane@users.noreply.github.com>
2023-06-06 14:11:16 -0300

    Update reajuste salarial.pas

--- 1º Bimestre/if else/reajuste salarial.pas
+++ 1º Bimestre/if else/reajuste salarial.pas
@@ -5,11 +5,10 @@ Begin
   if (salario < 500)
    then novo_salario := salario*1.15
    
-   else if (salario<=1000)
+   else if (salario <= 1000)
 	  then novo_salario := salario*1.05
 	  end;
 	end;
 
-	
 	write (novo_salario);  
 End.
