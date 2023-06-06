1bfaae709660294b2aad4233502d220e0ab915b0
Ysterllane <131874791+Ysterllane@users.noreply.github.com>
2023-06-06 14:25:20 -0300

    Update reajuste salarial.pas
    Comparação de variáveis com < e <=

--- 1º Bimestre/if else/reajuste salarial.pas
+++ 1º Bimestre/if else/reajuste salarial.pas
@@ -1,14 +1,17 @@
 Program Reajusta_Salario ;
   var novo_salario, salario: real;
 Begin
-  Read (salario);
-  if (salario < 500)
-   then novo_salario := salario*1.15
-   
-   else if (salario <= 1000)
-	  then novo_salario := salario*1.05
-	  end;
-	end;
-
-	write (novo_salario);  
+  write('Qual o seu salário? ');
+     Read (salario); 
+	writeln;
+  if (salario < 500)then 
+     begin
+	novo_salario := salario*1.15;
+     end   
+  else if (salario <= 1000) then 
+     begin
+	novo_salario := salario*1.05;
+     end;
+  write('Seu nomo salário eh: ', novo_salario:2:2); 
+     readkey; 
 End.
