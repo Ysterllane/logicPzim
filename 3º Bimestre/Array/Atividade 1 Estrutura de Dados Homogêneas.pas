4aa09e2872344f5f5b4456a021c5fc681b5f9009
Ysterllane <131874791+Ysterllane@users.noreply.github.com>
2023-08-22 09:11:05 -0300

    Update Atividade 1 Estrutura de Dados Homogêneas.pas

--- 3º Bimestre/Array/Atividade 1 Estrutura de Dados Homogêneas.pas
+++ 3º Bimestre/Array/Atividade 1 Estrutura de Dados Homogêneas.pas
@@ -1,1 +1,19 @@
+{Desenvolver um programa que efetue a leitura de 5 elementos de uma 
+matriz A tipo vetor. No final, apresente o total da soma de todos os 
+elementos que sejam ímpares.}
 
+Program Pzim ;
+var X:array[1..5]of integer;
+    i:integer;
+    soma:integer;
+Begin
+ soma:=0;
+ For i := 1 to 5 do
+ begin
+  Read(x[i]);  
+  writeln;
+  if(x[i] mod 2 = 1) then
+     soma:=soma+x[i];
+ End;
+ Writeln('Sua soma é: ',Soma);
+End.
